8e6fb0db9619db05358715290f9b6b65e7dcbc6f
dahalprastut <dahalprastut@gmail.com>
2019-05-12 03:19:49 +0545

    asdf

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 document.querySelector('.absolute-left').addEventListener('click' , ()=>{
     document.querySelector('body').classList.toggle('shownBox');
 });
+
+
+document.querySelector('.hiddens-map-button').addEventListener('click', ()=>{
+    alert('asdf');
+    console.log('asdf')
+    document.querySelector('.information').style.display="none";
+})
